8f50babac7e08700ad06a15b0a307e9b916e2a62
timibadass <omoyenitimi@gmail.com>
2020-11-29 13:33:31 +0100

    add htmlplugin

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 require('./assets/js/graphql');
 require('./assets/js/main');
-
 require('./assets/css/reset.css')
 require('./assets/css/style.css')
-
-const envVariables = process.env;
-window.envVariables = envVariables;
--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,26 @@ const webpack = require('webpack');
 const dotenv = require('dotenv').config({
     path: path.join(__dirname, '.env')
 });
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './app.js',
-    mode: 'development',
+    mode: 'production',
     output: {
         path: `${__dirname}/dist`,
         filename: 'bundle.js',
     },
     module: {
         rules: [{
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                },
+            },
+        }, {
             test: /\.css$/,
             use: [
                 'style-loader',
@@ -21,6 +31,10 @@ module.exports = {
         }, ],
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './index.html'),
+            filename: 'index.html',
+        }),
         new webpack.DefinePlugin({
             "process.env": JSON.stringify(dotenv.parsed)
         }),
